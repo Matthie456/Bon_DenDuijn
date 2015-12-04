ab8158a293e731cb6367d95743513c06cf1b6a26
XanderdenDuijn <xdenduijn@gmail.com>
2015-12-04 11:31:42 +0100

    test
    test

--- SpatialDecision/resources.py
+++ SpatialDecision/resources.py
@@ -2,7 +2,8 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.6)
+# Created: vr 4. dec 11:27:32 2015
+#      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
 
